31e07237601b864f1e6e8b62ba1d0d9851b6b698
luqman-samtia <bsf2004883@ue.edu.pk>
2024-09-28 15:23:03 +0500

    check error

--- routes/web.php
+++ routes/web.php
@@ -357,6 +357,7 @@ Route::prefix('super-admin')->middleware(['auth', 'xss', 'role:super_admin'])->g
     )->name('super.admin.dashboard');
     Route::get('/letters/search', [FormController::class, 'letter_search'])->name('letters.search');
     Route::get('/drafts/search', [FormController::class, 'draft_search'])->name('drafts.search');
+    Route::get('letter-form/{id}/preview', [FormController::class, 'preview'])->name('letter.previews');
 
     Route::get(
         '/total_letters',
